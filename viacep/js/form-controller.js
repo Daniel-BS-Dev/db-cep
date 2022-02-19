5ae2507d021b133955baef35d1fb0975bf39d031
Daniel-BS-Dev <danielbenedito355@gmail.com>
2022-02-19 12:35:44 -0300

    clear

--- viacep/js/form-controller.js
+++ viacep/js/form-controller.js
@@ -1,4 +1,10 @@
+import Address from './models.js'
+
+// funções construtura
 function State () {
+
+  this.address = new Address();
+
   this.cep = null;
   this.street = null;
   this.number = null;
@@ -12,8 +18,10 @@ function State () {
 
 }
 
+// instaciando
 const state = new State();
 
+// atribuindo valor
 export function init(){
     state.cep = document.forms.form.cep;
     state.street = document.forms.form.street;
@@ -23,5 +31,40 @@ export function init(){
     state.btnClear = document.forms.form.btnClear;
     state.errorCep = document.querySelector('[data-error="cep"]');
     state.errorNumber = document.querySelector('[data-error="number"]');
-    console.log(state)
+
+    state.number.addEventListener('change', handleNumberChange);
+    state.btnClear.addEventListener('click', handleClearValue);
+
+}
+
+// mensagem de error
+function handleNumberChange(event){
+    if(event.target.value == ''){
+        checkIsEmpty('number', 'Campo requerido');
+    }else{
+        checkIsEmpty('number','');
+    }
+}
+
+function checkIsEmpty(key, value){
+    const element = document.querySelector(`[data-error=${key}]`);
+    element.innerHTML = value;
+}
+
+// limpando o campo
+function handleClearValue(event){
+    event.preventDefault();
+    formClear();
+}
+
+function formClear(){
+    state.cep.value = '';
+    state.city.value = '';
+    state.street.value = '';
+    state.number.value = '';
+
+    checkIsEmpty('cep', '');
+    checkIsEmpty('number', '');
+
+    state.cep.focus();
 }
